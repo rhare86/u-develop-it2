7124f5ad7ea55d5afc9f89bbfaf8bc997402981a
rhare86 <rhare86@gmail.com>
2022-07-30 13:13:03 -0500

    connected to db

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use (express.json());
 
+// Connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'LifenSkillen1)',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 
